17065d9e1ecddaffbb2512a945b449129537cca0
Giang Nguyen <giangnguyen.tale@gmail.com>
2016-04-15 13:28:11 +0700

    add timber for logging

--- app/src/main/java/vn/tale/jars/BaseApp.java
+++ app/src/main/java/vn/tale/jars/BaseApp.java
@@ -2,12 +2,15 @@ package vn.tale.jars;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 import vn.tale.jars.di.AppComponent;
 import vn.tale.jars.di.DaggerAppComponent;
 import vn.tale.jars.model.ImmutableJar;
@@ -36,13 +39,47 @@ public abstract class BaseApp extends Application {
     component = builder.build();
     component.inject(this);
 
+    setupLogging();
+
     initialize();
+
+  }
+
+  private void setupLogging() {
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    } else {
+      Timber.plant(new CrashReportingTree());
+    }
   }
 
   private void initialize() {
     if (settings.isFirstRun()) {
-      jarRepository.add(getDefaultJars());
-      settings.setFirstRun(true);
+      jarRepository.add(getDefaultJars())
+          .subscribeOn(Schedulers.io())
+          .subscribe(integer -> {
+            Timber.d("number of item inserted is %d", integer);
+            settings.setFirstRun(true);
+          });
+    }
+  }
+
+  /** A tree which logs important information for crash reporting. */
+  private static class CrashReportingTree extends Timber.Tree {
+    @Override protected void log(int priority, String tag, String message, Throwable t) {
+      if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+        return;
+      }
+
+      FakeCrashLibrary.log(priority, tag, message);
+
+      if (t != null) {
+        if (priority == Log.ERROR) {
+          FakeCrashLibrary.logError(t);
+        } else if (priority == Log.WARN) {
+          FakeCrashLibrary.logWarning(t);
+        }
+      }
     }
   }
 
